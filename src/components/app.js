bd5eedb186f31a28a7a3f771f96e1f94a1aac025
Eugene Sokol <Eugenesokol97@gmail.com>
2017-07-13 12:18:59 +0200

    change 1

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import BookList from '../containers/Book_List'
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />  
+      </div>
     );
   }
 }
